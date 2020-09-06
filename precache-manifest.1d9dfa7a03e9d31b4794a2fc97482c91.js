71ca0a759ee574ded59072612aa3ea9f4bd35848
Jason Sohn <JasonSohn@Jasons-MacBook-Air.local>
2020-09-06 21:25:00 +0900

    Updates

---
+++ precache-manifest.1d9dfa7a03e9d31b4794a2fc97482c91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75fd9f782f18b5e2eb0837a051d09339",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "82f4582d634f2bd516f0",
+    "url": "/my-app/static/css/main.385afe39.chunk.css"
+  },
+  {
+    "revision": "d93fe400992f7dc65d30",
+    "url": "/my-app/static/js/2.2cb873af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.2cb873af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82f4582d634f2bd516f0",
+    "url": "/my-app/static/js/main.5dba9dbb.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
